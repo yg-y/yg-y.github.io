ba0cdf17b74418012c533a94567cb40f33c25650
young <y627756022@163.com>
2020-06-29 16:51:06 +0800

    update code.html

---
+++ js/data.js
@@ -0,0 +1,16 @@
+function getTags() {
+    let data = [
+        {
+            id: 1,
+            name: '后端'
+        },
+        {
+            id: 2,
+            name: '前端'
+        }, {
+            id: 3,
+            name: '数据库'
+        }
+    ]
+    return data;
+}
